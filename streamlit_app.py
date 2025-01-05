d9b271b1ad6b55d993d194236bd9f7aaa907e83c
charlieescalante <40127285+charlieescalante@users.noreply.github.com>
2025-01-05 13:52:20 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,17 @@ import streamlit as st
 st.header('ChatGPT - Open API')
 
 
+# initialize session state
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+#displays all chat history information
+# messages
+# {'role' : "user" or "assistant" or "system", "content" : prompt or response}
+for message in st.session_state.messages:
+    with st.chat_message(message['role']):
+        st.markdown(message['content'])
+
 # Chat Layout
 # Chat Input Kind of placeholder where user will unput his/her prompt
 # chat message : display all input message by user and respinse from assistant
